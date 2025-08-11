f3f32b0df6ee4a858c25b516fb007b87bef07da2
Stig Nygaard <stig@rockland.dk>
2025-08-11 10:32:28 +0200

    tune styling

--- static/peek.js
+++ static/peek.js
@@ -82,7 +82,9 @@ function setupFeedPeeker(el) {
                                         },
                                         itemDate(item)), (author ? ` - by ${author}` : '')
                                 ),
-                                cr('p', {class: 'item-content'}, item.content_text ?? stripHtml(item.content_html)));
+                                cr('p', {class: 'item-content'}, item.content_text ?? stripHtml(item.content_html)),
+                                cr('div', {class: 'item-categories'}, item.tags?.join(', '))
+                            );
                             popup.append(itemEl);
                         })
                     }
